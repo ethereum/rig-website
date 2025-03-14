256474e4d16d4d6ee8c51ff6e9a83027c9efab43
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2025-03-14 20:09:41 +0100

    feat: create section components

---
+++ components/ui/section.tsx
@@ -0,0 +1,65 @@
+import * as React from "react"
+
+import { cn } from "@/lib/utils"
+
+const Section = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
+>(({ className, ...props }, ref) => (
+  <section
+    ref={ref}
+    className={cn(
+      "space-y-8 py-12 sm:py-20",
+      className
+    )}
+    {...props}
+  />
+))
+Section.displayName = "Section"
+
+const SectionHead = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
+>(({ className, ...props }, ref) => (
+  <section
+    ref={ref}
+    className={cn(
+      "space-x-2",
+      className
+    )}
+    {...props}
+  />
+))
+SectionHead.displayName = "SectionHead"
+
+const SectionHeading = React.forwardRef<
+  HTMLHeadingElement,
+  React.HTMLAttributes<HTMLHeadingElement>
+>(({ className, ...props }, ref) => (
+  <h2
+    ref={ref}
+    className={cn(
+      "text-4xl text-primary font-sans lowercase inline",
+      className
+    )}
+    {...props}
+  />
+))
+SectionHeading.displayName = "SectionHeading"
+
+const SectionCounter = React.forwardRef<
+  HTMLSpanElement,
+  React.HTMLAttributes<HTMLSpanElement>
+>(({ className, children, ...props }, ref) => (
+  <span
+    ref={ref}
+    className={cn(
+      "font-sans",
+      className
+    )}
+    {...props}
+  >({children})</span>
+))
+SectionCounter.displayName = "SectionCounter"
+
+export { Section, SectionHead, SectionHeading, SectionCounter }
