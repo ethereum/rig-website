7169bb813970f39686e9e06ee4cb876188bde47b
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2025-04-02 15:53:15 -0600

    feat: create posts landing page

---
+++ app/posts/page.tsx
@@ -0,0 +1,23 @@
+import PostPreviewRow from "@/components/PostPreviewRow"
+import { fetchPosts } from "@/lib/posts"
+
+export default function Page() {
+  const posts = fetchPosts()
+
+  // TODO: Add pagination
+
+  return (
+    <main className="row-start-2">
+      <div>
+        {posts.map(({ frontmatter, path }) => (
+          <PostPreviewRow
+            key={path}
+            frontmatter={frontmatter}
+            href={path}
+            className="border-b"
+          />
+        ))}
+      </div>
+    </main>
+  )
+}
