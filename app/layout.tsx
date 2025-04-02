8c20845780e95e626590f916d0bef63d32a18bdb
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2025-04-02 07:58:05 -0600

    feat: update layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,13 @@
-import Image from "next/image"
 import type { Metadata } from "next"
 import { EB_Garamond, Montserrat } from "next/font/google"
-import "./globals.css"
+
 import Logo from "@/components/svg/rig-logo.svg"
+import GitHub from "@/components/svg/github.svg"
+import Twitter from "@/components/svg/twitter.svg"
+import Email from "@/components/svg/email.svg"
 import ThemeSwitch from "@/components/ThemeSwitch"
+
+import "./globals.css"
 
 const garamondSerif = EB_Garamond({
   variable: "--font-eb-garamond",
@@ -28,25 +32,29 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en" className="dark">
+    <html lang="en">
       <body
         className={`${garamondSerif.variable} ${montserratSans.variable} grid min-h-screen grid-rows-[auto_1fr_auto] items-center justify-items-center p-8 font-serif antialiased sm:px-18 sm:py-10`}
       >
         <header className="flex w-full justify-between">
-          <Image
-            src={Logo}
-            className="text-foreground text-7xl dark:invert"
+          <Logo
+            className="text-foreground text-[9rem]"
             alt="Robust Incentives Group RIG logo"
-            priority
-            width={150}
-            height={144}
           />
           <nav className="flex items-center gap-4">
             <ThemeSwitch />
           </nav>
         </header>
         {children}
-        <footer className="row-start-3 flex flex-wrap items-center justify-center gap-6"></footer>
+        <footer className="row-start-3 flex flex-wrap items-center justify-center gap-6 font-sans font-bold">
+          <div id="socials" className="flex items-center gap-x-4 text-3xl">
+            <span className="text-accent-foreground text-[50px]">&#91;</span>
+            <GitHub className="text-primary" />
+            <Twitter className="text-primary" />
+            <Email className="text-primary" />
+            <span className="text-accent-foreground text-[50px]">&#93;</span>
+          </div>
+        </footer>
       </body>
     </html>
   )
