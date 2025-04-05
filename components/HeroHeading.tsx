98aa9fd3cb7abf7ec1b7d4109b45045a4509ef0e
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2025-04-05 07:39:14 -0600

    feat: create HeroHeading component

---
+++ components/HeroHeading.tsx
@@ -0,0 +1,23 @@
+import { cn } from "@/lib/utils"
+
+type HeroHeadingProps = React.HTMLAttributes<HTMLHeadingElement> & {
+  prefix?: string
+}
+
+const HeroHeading = ({
+  children,
+  className,
+  prefix = "All",
+  ...props
+}: HeroHeadingProps) => {
+  return (
+    <h1 className={cn("-space-y-12 py-20", className)} {...props}>
+      <span className="block text-5xl leading-snug">{prefix}</span>{" "}
+      <span className="text-10xl text-accent-foreground leading-snug">
+        {children}
+      </span>
+    </h1>
+  )
+}
+
+export default HeroHeading
