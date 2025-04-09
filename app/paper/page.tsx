7f3f94c47ba790c632f0ab971c3940dac6e7700c
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2025-04-09 07:03:12 -0600

    feat: add papers fallback redirect

---
+++ app/paper/page.tsx
@@ -0,0 +1,16 @@
+"use client"
+
+import { useEffect } from "react"
+import { useRouter } from "next/navigation"
+
+import { PATH_PAPERS } from "@/lib/constants"
+
+export default function RedirectPage() {
+  const router = useRouter()
+
+  useEffect(() => {
+    router.replace(`/${PATH_PAPERS}s`)
+  }, [router])
+
+  return null
+}
