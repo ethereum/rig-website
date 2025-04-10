9721cf645ae34ed3b4f81ba00c217cc834246cb7
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2025-04-10 17:00:56 -0600

    temp: add dummy email filler

--- data/authors.ts
+++ data/authors.ts
@@ -6,35 +6,41 @@ export const authors: Author[] = [
     name: "Ansgar Dietrichs",
     avatar: "public/avatars/ansgar-dietrichs.jpg",
     twitter: "testing", // TODO: Get handles
+    email: "testing@testing.com", // TODO: Get emails
   },
   {
     id: "anders",
     name: "Anders",
     avatar: "public/avatars/anders.jpg",
     twitter: "testing", // TODO: Get handles
+    email: "testing@testing.com", // TODO: Get emails
   },
   {
     id: "barnabe",
     name: "Barnabé Monnot",
     avatar: "public/avatars/barnabe-monnot.jpg",
     twitter: "testing", // TODO: Get handles
+    email: "testing@testing.com", // TODO: Get emails
   },
   {
     id: "caspar",
     name: "Caspar Schwarz-Schilling",
     avatar: "public/avatars/caspar-schwarz-schilling.jpg",
     twitter: "testing", // TODO: Get handles
+    email: "testing@testing.com", // TODO: Get emails
   },
   {
     id: "michael",
     name: "Michael Neuder",
     avatar: "public/avatars/michael-neuder.jpg",
     twitter: "testing", // TODO: Get handles
+    email: "testing@testing.com", // TODO: Get emails
   },
   {
     id: "thomas",
     name: "Thomas Thiery",
     avatar: "public/avatars/thomas-thiery.jpg",
     twitter: "testing", // TODO: Get handles
+    email: "testing@testing.com", // TODO: Get emails
   },
 ]
