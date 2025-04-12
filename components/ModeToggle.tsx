1cff57c3a7ae997f0ac00ae679e0cf90b5087988
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2025-04-11 20:37:15 -0600

    fix: color mode toggle button

--- components/ModeToggle.tsx
+++ components/ModeToggle.tsx
@@ -23,17 +23,20 @@ export function ModeToggle() {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button
-          variant="outline"
-          size="icon"
-          className="border-none shadow-none"
-        >
+        <Button variant="empty" size="icon" className="relative">
+          <div
+            className="border-primary absolute inset-1.5 border-2"
+            style={{
+              clipPath:
+                "polygon(0 0, 100% 0, 100% 5px, 0 5px, 0 calc(100% - 5px), 100% calc(100% - 5px), 100% 100%, 0 100%)",
+            }}
+          />
           <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
           <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
+      <DropdownMenuContent align="end" className="rounded-none border-2">
         <DropdownMenuItem onClick={() => setTheme("light")}>
           Light
         </DropdownMenuItem>
