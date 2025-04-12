755b7df6b7dcd047da2e9cd7035be9715464fd77
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2025-04-12 12:04:20 -0600

    refactor: break out team member profiles

--- data/profiles.ts
+++ data/profiles.ts
@@ -1,46 +1,58 @@
 import type { Contributor } from "@/lib/types"
 
-export const profiles: Contributor[] = [
-  {
-    id: "ansgar",
-    name: "Ansgar Dietrichs",
-    avatar: "public/avatars/ansgar-dietrichs.jpg",
-    twitter: "testing", // TODO: Get handles
-    email: "testing@testing.com", // TODO: Get emails
-  },
+export const members: Contributor[] = [
   {
     id: "anders",
-    name: "Anders",
+    name: "Anders Elowsson",
     avatar: "public/avatars/anders.jpg",
     twitter: "testing", // TODO: Get handles
-    email: "testing@testing.com", // TODO: Get emails
-  },
-  {
-    id: "barnabe",
-    name: "Barnabé Monnot",
-    avatar: "public/avatars/barnabe-monnot.jpg",
-    twitter: "testing", // TODO: Get handles
-    email: "testing@testing.com", // TODO: Get emails
+    email: "anders.elowsson@ethereum.org",
   },
   {
     id: "caspar",
     name: "Caspar Schwarz-Schilling",
     avatar: "public/avatars/caspar-schwarz-schilling.jpg",
     twitter: "testing", // TODO: Get handles
-    email: "testing@testing.com", // TODO: Get emails
+    email: "caspar.schwarz-schilling@ethereum.org",
   },
   {
-    id: "michael",
-    name: "Michael Neuder",
-    avatar: "public/avatars/michael-neuder.jpg",
+    id: "davide",
+    name: "Davide Crapis",
+    avatar: undefined,
     twitter: "testing", // TODO: Get handles
-    email: "testing@testing.com", // TODO: Get emails
+    email: "davide.crapis@ethereum.org",
+  },
+  {
+    id: "julian",
+    name: "Julian Ma",
+    avatar: undefined,
+    twitter: "testing", // TODO: Get handles
+    email: "julian.ma@ethereum.org",
   },
   {
     id: "thomas",
     name: "Thomas Thiery",
     avatar: "public/avatars/thomas-thiery.jpg",
     twitter: "testing", // TODO: Get handles
-    email: "testing@testing.com", // TODO: Get emails
+    email: "thomas.thiery@ethereum.org",
+  },
+]
+
+export const profiles: Contributor[] = [
+  ...members,
+  {
+    id: "ansgar",
+    name: "Ansgar Dietrichs",
+    avatar: "public/avatars/ansgar-dietrichs.jpg",
+  },
+  {
+    id: "barnabe",
+    name: "Barnabé Monnot",
+    avatar: "public/avatars/barnabe-monnot.jpg",
+  },
+  {
+    id: "michael",
+    name: "Michael Neuder",
+    avatar: "public/avatars/michael-neuder.jpg",
   },
 ]
