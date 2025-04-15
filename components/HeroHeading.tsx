7486431c1f43ca825ff98722cde1ed0377a85dfa
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2025-04-15 07:44:47 -0600

    revert: hero prefix usage

--- components/HeroHeading.tsx
+++ components/HeroHeading.tsx
@@ -7,13 +7,17 @@ type HeroHeadingProps = React.HTMLAttributes<HTMLHeadingElement> & {
 const HeroHeading = ({
   children,
   className,
-  prefix = "All",
+  prefix,
   ...props
 }: HeroHeadingProps) => {
   return (
     <h1 className={cn("-space-y-12 py-20", className)} {...props}>
-      <span className="block text-4xl sm:text-5xl leading-snug">{prefix}</span>{" "}
-      <span className="text-9xl sm:text-10xl text-accent-foreground leading-snug">
+      {prefix && (
+        <span className="block text-4xl leading-snug sm:text-5xl">
+          {prefix}
+        </span>
+      )}
+      <span className="sm:text-10xl text-accent-foreground text-9xl leading-snug">
         {children}
       </span>
     </h1>
