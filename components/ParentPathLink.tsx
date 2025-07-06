d4366b611fd75db4736100a8fbfcfb152c3e44cd
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2025-07-06 11:04:43 -0700

    fix: apply not-found to all
    feat: add effective ".." link to not-found, refactor to use single not-found page component removing NotFoundPage component

---
+++ components/ParentPathLink.tsx
@@ -0,0 +1,23 @@
+"use client"
+
+import { usePathname } from "next/navigation"
+import { SquareArrowLeft } from "lucide-react"
+
+import Link from "@/components/ui/link"
+
+const ParentPathLink = () => {
+  const pathname = usePathname()
+  const parentPath = pathname.split("/").slice(0, -1).join("/")
+
+  return (
+    <Link
+      href={parentPath}
+      className="flex items-center font-sans hover:underline"
+    >
+      <SquareArrowLeft className="me-2 inline-block size-5" />
+      Go up to {parentPath}
+    </Link>
+  )
+}
+
+export default ParentPathLink
